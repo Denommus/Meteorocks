96c197946c63e88a453e826ae72ef2ba85a78852
Philip Moccio <philmoc@gmail.com>
2012-01-03 20:57:29 -0500

    Added animation capability for sprites and an animation for the ship's rockets.

--- Icone2DLibrary/Objects/SpriteStruct/Sprite.cs
+++ Icone2DLibrary/Objects/SpriteStruct/Sprite.cs
@@ -15,12 +15,43 @@ namespace Icone2DLibrary.Objects.SpriteStruct
         public Vector2 position;
         public Vector2 origin;
         public float depth;
-
+        public float animationTime;
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, position, new Rectangle(0, 0, texture.Width, texture.Height),
                 Color.White, rotation, origin, scale, SpriteEffects.None, depth);
+        }
+
+        //Plays an animation from a given list of Texture2D's
+        public void Animate(List<Texture2D> textures, float time, float animationRate)
+        {
+            //Keep track of the elapsed time 
+            if (animationTime == null)
+            {
+                animationTime = time;
+            }
+            else
+            {
+                animationTime += time;
+            }
+
+            //The given animation rate determines when we switch to the next texture in the list
+            if (animationTime > animationRate / textures.Count)
+            {
+                //Switch to the next texture in the list, or the first texture if we're at the end. 
+                if (textures.IndexOf(texture) < textures.Count - 1)
+                {
+                    texture = textures[textures.IndexOf(texture) + 1];
+                }
+                else
+                {
+                    texture = textures[0];
+                }
+
+                //Reset the elapsed time
+                animationTime = 0;
+            }
         }
     }
 }
